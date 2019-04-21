9391877c47fe0cb111af32732fe7d8e97afa05b3
Apple <hiennpps06647@fpt.edu.vn>
2019-04-21 23:22:24 +0700

    hien commit

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,13 @@
 
 $(document).ready(function () {
 	// All script write here
+	const ProjectPhotoSwiper = new Swiper('.history .swiper-container', {
+		slidesPerView: 1,
+		loop: true,
+		spaceBetween: 30,
+		navigation: {
+			prevEl: '.history .lnr.lnr-chevron-left',
+			nextEl: '.history .lnr.lnr-chevron-right'
+		}
+	})
 });
